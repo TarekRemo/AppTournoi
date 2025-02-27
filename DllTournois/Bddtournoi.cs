70c5d1f22512cbc8540a30e2434863140609d141
tarek <tarekremo64@mail.com>
2025-02-27 15:58:37 +0100

    création de la vue principale et des vue de connexion, et de bdd

--- DllTournois/Bddtournoi.cs
+++ DllTournois/Bddtournoi.cs
@@ -1,7 +1,42 @@
-﻿namespace DllTournois
+﻿
+using BddtournoiContext; 
+
+namespace DllTournois
 {
     public class Bddtournoi
     {
+        //paramètres de connexion à la BDD
+        private static string host = "localhost";
+        private static string user = "adminTournois";
+        private static string password = "Password1234@";
+        private static string dataBase = "bddtournois";
+        private static string port = "3306"; 
 
+        //objet BDD du modèle linQ
+        private BddtournoiDataContext DB;
+
+        //instance du singleton
+        private static Bddtournoi instance; 
+
+        private Bddtournoi()
+        {
+            DB = new BddtournoiDataContext("User Id=" + user + ";Password=" + password + ";Host=" + host +
+                                                ";Port=" + port + ";Database=" + dataBase + ";Persist Security Info=True");
+        }
+
+        public Bddtournoi getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Bddtournoi(); 
+            }
+            return instance;
+        }
+
+
+        public List<Tournoi> GetTournois()
+        {
+            return DB.Tournois.ToList();
+        }
     }
 }
